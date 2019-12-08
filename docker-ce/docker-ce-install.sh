abf0520121775c5d8108875e22cc7c956e212e25
陶辰 <chen.tao@ifchange.com>
2019-12-08 14:11:48 +0800

    update

--- docker-ce/docker-ce-install.sh
+++ docker-ce/docker-ce-install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo_bak
-
-curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-
+#mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo_bak
+#
+#curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+#
 curl -o /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel.repo
 
-yum makecache
 yum install -y yum-utils
 
 yum install wget net-tools telnet tree nmap sysstat lrzsz dos2unix bind-utils -y
